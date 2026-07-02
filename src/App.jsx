97118cc9c857a1307b9fc51f4f309f34629e587f
araae <araliae@icloud.com>
2026-07-02 09:06:45 -0700

    updated app.jsx with all routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import { Route, Routes } from "react-router";
 import Layout from "./layout/Layout";
 import Login from "./auth/Login";
 import Register from "./auth/Register";
+import WelcomePage from "./pages/WelcomePage";
+import SearchPage from "./pages/Search";
 import DogPage from "./pages/DogPage";
+import UserPage from "./pages/UserPage";
+import Messages from "./pages/Messages";
 
 /** Import the new user profile page*/
 import UserPage from "./pages/UserPage";
@@ -14,9 +18,11 @@ export default function App() {
         <Route index element={<p>Home page</p>} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/search" element={<SearchPage />} />
         <Route path="/profile" element={<UserPage />} />
         <Route path="/users/:id" element={<UserPage />} />
         <Route path="/dogs/:dogId" element={<DogPage />} />
+        <Route path="/messages" element={<Messages />} />
       </Route>
     </Routes>
   );
